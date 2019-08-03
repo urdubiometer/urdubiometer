c1d4b5fac09d93706371477b40e46e2d35c2f6ab
seanpue <pue@msu.edu>
2019-08-02 22:40:11 -0400

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/urdubiometer/urdubiometer',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
--- urdubiometer/__init__.py
+++ urdubiometer/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """A. Sean Pue"""
 __email__ = 'a@seanpue.com'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 __all__ = ['scanner']
 
